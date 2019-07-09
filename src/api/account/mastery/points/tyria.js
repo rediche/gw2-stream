72bebe31fbd2bdf0dc13f8e322ea27788d1e9c02
Marcus Geil <weblrmail@gmail.com>
2019-07-09 21:43:18 +0200

    Add endpoint: Tyria mastery

---
+++ src/api/account/mastery/points/tyria.js
@@ -0,0 +1,23 @@
+const api = require("../../../../api");
+
+module.exports = (req, res) => {
+  if (!req.query.token) {
+    res.end("No API token provided.");
+  }
+
+  api
+    .authenticate(req.query.token)
+    .account()
+    .mastery()
+    .points()
+    .get()
+    .then(masteries => {
+      const tyria = masteries.totals.reduce((total, region) => {
+        return region.region !== "Tyria" ? total : (total += region.spent);
+      }, 0);
+      res.end(tyria.toString());
+    })
+    .catch(error => {
+      res.end("Couldn't load the total mastery points.");
+    });
+};
