19ce3f0e223822b4c4249d300853f0f3e36eed67
Marcus Geil <weblrmail@gmail.com>
2019-07-09 21:51:19 +0200

    Add endpoint: Fractal level

---
+++ src/api/account/fractal-level.js
@@ -0,0 +1,18 @@
+const api = require("../../api");
+
+module.exports = (req, res) => {
+  if (!req.query.token) {
+    res.end("No API token provided.");
+  }
+
+  api
+    .authenticate(req.query.token)
+    .account()
+    .get()
+    .then(account => {
+      res.end(account.fractal_level.toString());
+    })
+    .catch(error => {
+      res.end("Couldn't load the fractal level.");
+    });
+};
