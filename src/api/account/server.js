444441721c6eb551be519698368f3095cb05fcba
Marcus Geil <weblrmail@gmail.com>
2019-07-09 20:44:08 +0200

    Add endpoint: Account Server

---
+++ src/api/account/server.js
@@ -0,0 +1,17 @@
+const api = require('../../api');
+
+module.exports = async (req, res) => {
+  if (!req.query.token) {
+    res.end("No API token provided.");
+  }
+
+  api.authenticate(req.query.token);
+
+  try {
+    const account = await api.account().get();
+    const world = await api.worlds().get(account.world);
+    res.end(world.name);
+  } catch (error) {
+    res.end("Couldn't load server.");
+  }
+};
